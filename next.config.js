28460e1e7d4ebb9c126e000c9280c6fab8db06cc
Xavier Damman <xdamman@gmail.com>
2022-10-13 13:39:04 +0200

    poca.earth

--- next.config.js
+++ next.config.js
@@ -257,6 +257,108 @@ module.exports = {
         permanent: false,
         destination: "https://regensunite.co/bogota",
       },
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "drive.allforclimate.earth",
+          },
+        ],
+        permanent: false,
+        destination:
+          "https://drive.google.com/drive/u/0/folders/1g14Qyf_DmvGuevk4Ks5NgfkWPN5V6H6O",
+      },
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "join.allforclimate.earth",
+          },
+        ],
+        permanent: false,
+        destination:
+          "https://jz04xmgcexm.typeform.com/to/lPbcL9nj?typeform-source=join.allforclimate.earth",
+      },
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "zoom.allforclimate.earth",
+          },
+        ],
+        permanent: false,
+        destination: "https://us02web.zoom.us/j/6025635806",
+      },
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "dework.allforclimate.earth",
+          },
+        ],
+        permanent: false,
+        destination: "https://dework.xyz/o/all-for-clim-5YSFCGX71LR0qVyvqsL2w5",
+      },
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "bounties.allforclimate.earth",
+          },
+        ],
+        permanent: false,
+        destination: "https://dework.xyz/o/all-for-clim-5YSFCGX71LR0qVyvqsL2w5",
+      },
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "tasks.allforclimate.earth",
+          },
+        ],
+        permanent: false,
+        destination: "https://dework.xyz/o/all-for-clim-5YSFCGX71LR0qVyvqsL2w5",
+      },
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "projects.allforclimate.earth",
+          },
+        ],
+        permanent: false,
+        destination: "https://dework.xyz/o/all-for-clim-5YSFCGX71LR0qVyvqsL2w5",
+      },
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "calendar.allforclimate.earth",
+          },
+        ],
+        permanent: false,
+        destination:
+          "https://calendar.google.com/calendar/u/0/embed?src=c_kcbdb0ulem2nivoiugvfbmhjb8@group.calendar.google.com",
+      },
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "discord.allforclimate.earth",
+          },
+        ],
+        permanent: false,
+        destination: "https://discord.gg/7Cb6Nf2MgM",
+      },
     ];
   },
   async rewrites() {
