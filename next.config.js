9ef9e374a7ed07880ab9c29bdc393801d6b3cfc4
Xavier Damman <xdamman@gmail.com>
2023-06-14 01:03:57 +0200

    deck.citizenwallet.xyz

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const subdomainsRedirections = {
       "https://signal.group/#CjQKIK7lRZGN89uFUJqZ31npeVKbVHfo5Z15yl9XFoAeh2OrEhDfnmWNlW98l-OpBKzAnPET",
     drive: "https://drive.google.com/open?id=16fm3DNfOsL7HJ_uz4DuL9krNRh72YoRS",
     miro: "http://miro.com/app/board/uXjVMd0Js_E=/",
-    deck: "https://drive.google.com/open?id=1IduWyKuKF6YFnuLsGJKIVBrfbRSpzYyg",
+    deck: "https://drive.google.com/open?id=1J7HjSNKAMFic2tWDQqix8x1opD_0jpyl",
   },
   "citizencorner.brussels": {
     calendar:
