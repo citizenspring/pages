b47d0feb2c302ef56f984e5ef18a8571820ef6bf
Xavier Damman <xdamman@gmail.com>
2023-10-23 12:01:47 +0200

    photos.dao.brussels

--- next.config.js
+++ next.config.js
@@ -44,6 +44,7 @@ const subdomainsRedirections = {
     zoom: "https://us02web.zoom.us/j/6025635806",
     figma: "https://www.figma.com/files/team/1288058715406744670/daobrussels",
     telegram: "https://t.me/joinchat/5NbP0-Vl5Vg3MTgx",
+    photos: "https://photos.app.goo.gl/f921RyUyJAEAUy7S8",
     calendar:
       "https://calendar.google.com/calendar/embed?src=haijn9je0u2ci9efj7g0it8tk4%40group.calendar.google.com&ctz=Europe%2FBrussels",
   },
