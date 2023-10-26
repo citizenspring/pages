9a99924243e3d42380249a8b9034ca65942128cc
Xavier Damman <xdamman@gmail.com>
2023-10-26 23:13:56 +0200

    Added optimism for <Balance />

--- pages/api/balance.js
+++ pages/api/balance.js
@@ -85,6 +85,12 @@ const tokenContractAddresses = {
       decimals: 6,
     },
   },
+  optimism: {
+    DAI: {
+      address: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+      decimals: 18,
+    },
+  },
 };
 
 const apihosts = {
