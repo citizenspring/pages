ae548b6300d099d2b50ddaa8e56c97a0d27f1c73
Xavier Damman <xdamman@gmail.com>
2024-09-18 18:31:13 +0200

    fix x.com embeds

--- components/TwitterEmbed.js
+++ components/TwitterEmbed.js
@@ -4,7 +4,12 @@ export default ({ tweetUrl }) => {
   return (
     <div className="tweet" style={{ maxWidth: "560px", margin: "0 auto" }}>
       <blockquote className="twitter-tweet">
-        <a href={`${tweetUrl}?ref_src=twsrc%5Etfw`}></a>
+        <a
+          href={`${tweetUrl.replace(
+            "x.com/",
+            "twitter.com/"
+          )}?ref_src=twsrc%5Etfw`}
+        ></a>
       </blockquote>
       <script async src="https://platform.twitter.com/widgets.js"></script>
     </div>
